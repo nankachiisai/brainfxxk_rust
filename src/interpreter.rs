28b174b4b5761e3bcf4cfa23011852bec5039e27
yasuda0102 <webnuac@gmail.com>
2024-04-11 20:46:22 +0900

    インタプリタ部分を分割

---
+++ src/interpreter.rs
@@ -0,0 +1,109 @@
+use core::fmt;
+use std::error;
+
+#[derive(Debug, Clone)]
+pub struct Interpreter {
+    p_input: usize,    // input pointer
+    m_input: Vec<u8>,  // input memory
+    p_output: usize,   // output pointer
+    m_output: Vec<u8>, // output memory
+    p_inst: usize,     // instruction pointer
+    m_inst: Vec<u8>,   // instruction memory
+    p_data: usize,     // data pointer
+    m_data: Vec<u8>,   // data memory
+}
+
+impl Interpreter {
+    // 構造体を初期化する
+    pub fn new(instruction: &str, input: &str) -> Interpreter {
+        Interpreter {
+            p_input: 0,
+            m_input: Vec::from(input.as_bytes()),
+            p_output: 0,
+            m_output: vec![0; 1000],
+            p_inst: 0,
+            m_inst: Vec::from(instruction.as_bytes()),
+            p_data: 0,
+            m_data: vec![0; 1000],
+        }
+    }
+
+    // brainfxxkコードを与えられた入力に従って実行する
+    pub fn run(&mut self) -> Result<String, InterpretError> {
+        loop {
+            // for debug
+            // println!("i_memory[{}]: {}", ip, i_memory[ip] as char);
+            // println!("d_memory[{}]: {}", dp, d_memory[dp]);
+            // println!("");
+    
+            match self.m_inst[self.p_inst] {
+                b'>' => self.p_data += 1,                 // increment pointer
+                b'<' => self.p_data -= 1,                 // decrement pointer
+                b'+' => self.m_data[self.p_data] += 1,       // increment value
+                b'-' => self.m_data[self.p_data] -= 1,       // decrement value
+                b'.' => {                        // output value
+                    self.m_output[self.p_output] = self.m_data[self.p_data];
+                    self.p_output += 1;
+                },
+                b',' => {                        // input value
+                    self.m_data[self.p_data] = self.m_input[self.p_input];
+                    self.p_input += 1;
+                },
+                b'[' => {                        // jump forward
+                    if self.m_data[self.p_data] == 0 {
+                        while self.m_inst[self.p_inst] != b']' {
+                            self.p_inst += 1;
+                        }
+                        self.p_inst += 1;
+                    }
+                },
+                b']' => {                        // jump backward
+                    if self.m_data[self.p_data] != 0 {
+                        while self.m_inst[self.p_inst] != b'[' {
+                            self.p_inst -= 1;
+                        }
+                        self.p_inst -= 1;
+                    }
+                },
+                _ => (),
+            }
+
+            // インストラクションポインタを進める
+            // 最後まで実行したら終了
+            if self.m_inst.len() - 1 == self.p_inst {
+                break;
+            } else {
+                self.p_inst += 1;
+            }
+        }
+    
+        let ret = String::from_utf8(self.m_output.clone()).unwrap();
+        Ok(ret)
+    }
+}
+
+// インタープリタ実行時のエラーを表す構造体
+#[derive(Debug, Clone)]
+pub struct InterpretError {
+    message: String,
+}
+
+impl InterpretError {
+    fn new(message: &str) -> InterpretError {
+        InterpretError {
+            message: message.to_string(),
+        }
+    }
+}
+
+impl error::Error for InterpretError {
+    fn description(&self) -> &str {
+        &self.message
+    }
+}
+
+impl fmt::Display for InterpretError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Error: {}", self.message)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use core::fmt;
-use std::error;
 use std::fs::File;
 use std::io::Read;
 use std::env;
+
+mod interpreter;
+use interpreter::Interpreter;
 
 fn main() {
     // コマンドライン引数を取得
@@ -21,7 +22,8 @@ fn main() {
     };
 
     // 実行する
-    match run(&code, "test") {
+    let mut ipreter = Interpreter::new(code.as_str(), "test");
+    match ipreter.run() {
         Ok(_output) => println!("{}", _output),
         Err(e_run) => panic!("Interpreter Error: {}", e_run),
     }
@@ -41,105 +43,4 @@ fn read_file(file_path: &str) -> Result<String, std::io::Error> {
 fn get_command_line_arguments() -> Vec<String> {
     let args: Vec<String> = env::args().collect();
     args.to_vec()
-}
-
-// インタープリタ実行時のエラーを表す構造体
-#[derive(Debug, Clone)]
-struct InterpretError {
-    message: String,
-}
-
-impl InterpretError {
-    fn new(message: &str) -> InterpretError {
-        InterpretError {
-            message: message.to_string(),
-        }
-    }
-}
-
-impl error::Error for InterpretError {
-    fn description(&self) -> &str {
-        &self.message
-    }
-}
-
-impl fmt::Display for InterpretError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Error: {}", self.message)
-    }
-}
-
-// brainfxxkコードを実行する関数
-fn run(code: &str, input: &str) -> Result<String, InterpretError> {
-    // input pointer
-    let mut inp = 0;
-
-    // input memory
-    let input: Vec<u8> = Vec::from(input.as_bytes());
-
-    // output pointer
-    let mut oup = 0;
-
-    // output memory
-    let mut output: Vec<u8> = vec![0; 1000];
-
-    // instruction pointer
-    let mut ip = 0;
-
-    // instruction memory
-    let i_memory: Vec<u8> = Vec::from(code.as_bytes());
-
-    // data pointer
-    let mut dp = 0;
-
-    // data memory
-    let mut d_memory: Vec<u8> = vec![0; 1000];
-
-    loop {
-        // for debug
-        // println!("i_memory[{}]: {}", ip, i_memory[ip] as char);
-        // println!("d_memory[{}]: {}", dp, d_memory[dp]);
-        // println!("");
-
-        match i_memory[ip] {
-            b'>' => dp += 1,                 // increment pointer
-            b'<' => dp -= 1,                 // decrement pointer
-            b'+' => d_memory[dp] += 1,       // increment value
-            b'-' => d_memory[dp] -= 1,       // decrement value
-            b'.' => {                        // output value
-                output[oup] = d_memory[dp];
-                oup += 1;
-            },
-            b',' => {                        // input value
-                d_memory[dp] = input[inp];
-                inp += 1;
-            },
-            b'[' => {                        // jump forward
-                if d_memory[dp] == 0 {
-                    while i_memory[ip] != b']' {
-                        ip += 1;
-                    }
-                    ip += 1;
-                }
-            },
-            b']' => {                        // jump backward
-                if d_memory[dp] != 0 {
-                    while i_memory[ip] != b'[' {
-                        ip -= 1;
-                    }
-                    ip -= 1;
-                }
-            },
-            _ => (),
-        }
-
-        if i_memory.len() - 1 == ip {
-            break;
-        } else {
-            ip += 1;
-        }
-    }
-
-    let ret = String::from_utf8(output).unwrap();
-    Ok(ret)
 }
